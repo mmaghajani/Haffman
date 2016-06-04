a3e1fa33579d19f29e9d1776d5a8bac9ce4bd21c
mmaghajani <mma137421@gmail.com>
2016-06-04 20:35:56 +0430

    Adds encoding method

--- src/core/HaffmanHandler.java
+++ src/core/HaffmanHandler.java
@@ -1,10 +1,108 @@
 package core;
 
-import java.io.File;
+import core.tree.BinaryTree;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 /**
  * Created by mma on 6/4/2016.
  */
 public class HaffmanHandler {
-    private File file ;
+    private File file;
+    private HashMap<Character, Integer> numOfChars;
+    private HashMap<Character, String> codes;
+
+    public HaffmanHandler(File file) {
+        codes = new HashMap<>() ;
+        this.file = file;
+        numOfChars = getNumOfCharsFromContent(getContentFromFile());
+        BinaryTree treeOfFile = BinaryTree.createHaffmanTree(numOfChars);
+        generateCodesForChars(treeOfFile);
+    }
+
+    public File encode() {
+        if (file == null) {
+            try {
+                throw new FileNotFoundException("Please select a file for encoding");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        } else {
+            return convertToHaffmanCode();
+        }
+    }
+
+    private File convertToHaffmanCode() {
+        String s = "HaffmanEncoded";
+        s += file.getName();
+        s += ".txt";
+        File encodedFile = new File("../encodedFile/" + s);
+        if (encodedFile.exists()) {
+            return encodedFile;
+        } else {
+            String content = getContentFromFile();
+            String extension = "";
+            for (int i = 0; i < content.length(); i++) {
+                extension += codes.get(content.charAt(i));
+            }
+
+            try {
+                FileWriter writer = new FileWriter(encodedFile);
+                writer.write(extension);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return encodedFile;
+        }
+    }
+
+    private void generateCodesForChars(BinaryTree treeOfFile) {
+        dfs(treeOfFile.getRoot(), "");
+    }
+
+    private void dfs(BinaryTree.Node root, String code) {
+        if (root.getLeft() == null) {
+            char c = root.getLabel().charAt(0);
+            codes.put(c, code);
+        } else {
+            dfs(root.getLeft(), code + "0");
+            dfs(root.getRight(), code + "1");
+        }
+    }
+
+    private HashMap<Character, Integer> getNumOfCharsFromContent(String content) {
+        HashMap<Character, Integer> numOfChars = new HashMap<>();
+        for (int i = 0; i < content.length(); i++) {
+            char c = content.charAt(i);
+            if (numOfChars.keySet().contains(c)) {
+                int x = numOfChars.get(c);
+                x++;
+                numOfChars.replace(c, x);
+            } else {
+                numOfChars.put(c, 1);
+            }
+        }
+
+        return numOfChars;
+    }
+
+    private String getContentFromFile() {
+        String content = "";
+        try {
+            for (String string : Files.readAllLines(Paths.get(file.getPath()))) {
+                content += string;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content;
+    }
 }
--- src/core/tree/BinaryTree.java
+++ src/core/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package core.tree;
 
+import java.util.HashMap;
+
 /**
  * Created by mma on 6/4/2016.
  */
@@ -9,4 +11,46 @@ public class BinaryTree {
     public BinaryTree(){
 
     }
+
+    public static BinaryTree createHaffmanTree(HashMap<Character , Integer> numOfChars ){
+
+        return null ;
+    }
+
+    public Node getRoot(){
+        return root ;
+    }
+
+    public class Node {
+        private CharSequence label ;
+        private int number ;
+        private Node left ;
+        private Node right ;
+
+        public Node(CharSequence label , int number ){
+            this.label = label ;
+            this.number = number ;
+        }
+
+        public Node getLeft(){
+            return left ;
+        }
+
+        public Node getRight(){
+            return right ;
+        }
+
+        public void setLeft(Node node){
+            left = node ;
+        }
+
+        public void setRight( Node node ){
+            right = right ;
+        }
+
+        public CharSequence getLabel(){
+            return label ;
+        }
+    }
+
 }
